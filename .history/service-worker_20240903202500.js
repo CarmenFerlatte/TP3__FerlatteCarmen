a451f8b2082d77cd65a17e78a00b6f036f200a8a
Carmen Ferlatte <carmen.ferlatte@gmail.com>
2024-09-03 23:05:50 -0400

    révision

---
+++ .history/service-worker_20240903202500.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'stack-cache-v1';
+
+const FILES_TO_CACHE = [
+    './offline.html',
+    './index.html',
+    './realisations.html',
+    './contact.html',
+    './assets/php/header.html',
+    './assets/php/footer.html',
+    './img/icons/icon-192x192.png',
+    './style/css/style.css',
+    './img/gifs/courriel_dino.gif',
+    './img/gifs/fleche_118.gif',
+    
+];
+
+// Installation du Service Worker
+self.addEventListener('install', (evt) => {
+    console.log('[ServiceWorker] Install');
+    // Mise en cache des fichiers de l'application
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Mise en cache des fichiers de l\'application');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+// Activation du Service Worker
+self.addEventListener('activate', (evt) => {
+    console.log('[ServiceWorker] Activate');
+
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Retirer la vieille cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+
+    self.clients.claim();
+});
+
+// Accès à une ressource mise en cache
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    // Gestion de l'évènement Fetch (accès à une ressource mise en cache)
+    if (evt.request.mode !== 'navigate') {
+        return;
+    }
+
+    evt.respondWith(
+        fetch(evt.request).catch(() => {
+            return caches.open(CACHE_NAME).then((cache) => {
+                return cache.match('offline.html');
+            });
+        })
+    );
+});
